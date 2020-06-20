56569042feff775b7b4ce39a0881567176c52a64
Sourav Kunda <souravkunda00@gmail.com>
2020-06-20 21:12:03 +0530

    data and location added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import Layout from "./components/Layout/Layout";
+import Location from "./components/Location/Location";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Layout></Layout>
+        <Layout>
+          <Location />
+        </Layout>
       </div>
     </BrowserRouter>
   );
